458bde851d682d3be309dca8f251f970f8b63d09
jackeratarina <duongmandat003@gmail.com>
2019-08-16 14:34:27 +0700

    first commit

---
+++ src/main/java/com/group1/fimnew/FimnewApplication.java
@@ -0,0 +1,13 @@
+package com.group1.fimnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FimnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FimnewApplication.class, args);
+	}
+
+}
